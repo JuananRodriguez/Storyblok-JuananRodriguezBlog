d8cae4198e0cd1df943d593922de17556bc3b662
juanan_plexus <juan.rodriguezmartinez@plexus.es>
2021-03-30 18:10:55 +0200

    init storyblok example

---
+++ components/Teaser.js
@@ -0,0 +1,26 @@
+import React from "react";
+import SbEditable from "storyblok-react";
+
+const Teaser = ({ blok }) => {
+  if (blok.image) {
+    return (
+      <SbEditable content={blok}>
+        <div className="bg-white-half">
+          <div className="pb-6 pt-16 container mx-auto">
+            <h2 className="text-6xl font-bold font-serif text-primary">
+              {blok.headline}
+            </h2>
+            <img
+              src={blok.image.filename}
+              alt={blok.image.alt}
+              className="w-full"
+            />
+          </div>
+        </div>
+      </SbEditable>
+    );
+  }
+  return null;
+};
+
+export default Teaser;
