be72f6e16325123ab34d9425718d55490def4c3f
Juanan Rodriguez <hola@juananrodriguez.es>
2021-04-25 00:09:25 +0200

    including a first version of the syle components css styling theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import { ThemeProvider } from 'styled-components'
 import '../styles/tailwind.css'
 
+const theme = {
+  breakpoints: {
+    m: "1040px",
+    l: "1200px",
+  },
+  colors: {
+    title: '#0070f3', 
+    text: '#040404',
+  },
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
