2ff7cf9ec9321091041aa00d7c83f2d86f187def
Juanan Rodriguez <hola@juananrodriguez.es>
2021-04-24 21:10:02 +0200

    stylised component section

--- components/Section.js
+++ components/Section.js
@@ -1,16 +1,27 @@
 import React from "react";
 import SbEditable from "storyblok-react";
+import styled from "styled-components";
 import DynamicComponent from './DynamicComponent'
 
+
+
+const SectionStyled = styled.section`${({ isFullwidth, isFullpage }) => `
+    position: relative;
+    max-width: ${!isFullwidth ? "1200px" : "100%"};
+    margin: auto;
+    min-height: ${isFullpage ? "100vh" : "auto"};
+`}
+`
+
 const Section = ({ blok }) => {
-    const { _uid, bloks } = blok
+    const { _uid, bloks, fullwidth, fullpage } = blok
     return (
         <SbEditable content={blok} key={_uid}>
-            <section>
+            <SectionStyled isFullwidth={fullwidth} isFullpage={fullpage}>
                 {
                     bloks.map((nestedBlok) => <DynamicComponent blok={nestedBlok} />)
                 }
-            </section>
+            </SectionStyled>
         </SbEditable>
     );
 };
