2f0e2ae6dd96aff2e735c6385431c28163d13f6e
Juanan Rodriguez <hola@juananrodriguez.es>
2021-04-24 20:41:52 +0200

    stylised component title

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,46 @@
 import React from "react";
 import SbEditable from "storyblok-react";
+import styled, { css } from "styled-components";
+
+
+const CommonStyles = css`
+    font-weight: 900;
+    text-transform: uppercase;
+    margin: 0;
+    padding: 0;
+    line-height: 1.1;
+    letter-spacing: .8px;
+    font-size: 120px;
+    display: block;
+
+    @media (max-width: 1200px){
+        font-size: 90px;
+    }
+
+    @media (max-width: 1040px){
+        font-size: 60px;
+    }
+`
+
+const ContentSpanStyled = styled.span`
+    ${CommonStyles}
+    -webkit-text-stroke: 2px #434343;
+    color: transparent;
+`;
+
+const SubtitleSpanStyled = styled.span`
+    ${CommonStyles}
+    color: #434343;
+`;
 
 const Title = ({ blok }) => {
     const { _uid, content, subtitle } = blok
     return (
         <SbEditable content={blok} key={_uid}>
-            <h1>{content}{subtitle}</h1>
+            <h1>
+                <ContentSpanStyled>{content}</ContentSpanStyled>
+                <SubtitleSpanStyled>{subtitle}</SubtitleSpanStyled>
+            </h1>
         </SbEditable>
     );
 };
