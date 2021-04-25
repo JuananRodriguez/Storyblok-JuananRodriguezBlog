0afdaa3620a4a80adac46f522e3adb4684aee581
Juanan Rodriguez <hola@juananrodriguez.es>
2021-04-25 13:15:27 +0200

    updated paragraph size for breakpoint M

--- styled/ParagraphStyled.styled.js
+++ styled/ParagraphStyled.styled.js
@@ -4,6 +4,11 @@ const ParagraphStyled = styled.p`${({ cssFromStoryBlok, theme }) => `
         font-size: 1.5rem;
         color: ${theme.colors.text};
         margin-bottom: 1rem;
+
+        @media (max-width: ${theme.breakpoints.m}){
+            font-size: 1.1rem;
+        }
+
         ${cssFromStoryBlok}
     `}
 `
