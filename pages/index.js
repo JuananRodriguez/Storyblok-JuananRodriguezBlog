3a07164fc12eea0d9e33be239f574845c289411f
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-08 20:33:13 +0200

    used getServerSideProps instead of getInitialProps to improve performance

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,29 @@ import Layout from '../components/Layout'
 import StoryblokService from '../utils/storyblok-service'
 import useStoryblokEditor from '../hooks/useStoryblokEditor'
 
-const EntryPoint = (props) => {
-  const [story] = useStoryblokEditor(props.res.data.story);
+const EntryPoint = ({ res, language }) => {
+  const [story] = useStoryblokEditor(res.data.story);
   return (
-    <Layout language={props.language}>
+    <Layout language={language}>
       <Page content={story.content} />
     </Layout>
   )
 }
 
-EntryPoint.getInitialProps = async ({ query }) => {
+export async function getServerSideProps({ query }) {
   StoryblokService.setQuery(query)
   let language = query.language || "en"
   let insertLanguage = language !== "en" ? `/${language}` : ""
   let res = await StoryblokService.get(`cdn/stories${insertLanguage}/home`,
     {
-      "resolve_relations": "portfolio-grid.works"
+      "resolve_relations": "portfolio-grid.items"
     })
 
   return {
-    res,
-    language
+    props: {
+      res,
+      language
+    }
   }
 }
 
