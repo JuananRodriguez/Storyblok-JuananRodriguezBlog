63eab930bfb323267d7cf7938a24b6d4063676cf
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-09 17:15:54 +0200

    created TipPost Page

--- components/TipPost.js
+++ components/TipPost.js
@@ -1,22 +1,22 @@
 import React from "react";
 import SbEditable from "storyblok-react";
-import { render } from "storyblok-rich-text-react-renderer";
+import RichText from "../molecules/RichText";
+import Styled from "styled-components";
+
+const Section = Styled.section`
+    max-width: 1000px;
+    margin: auto;
+    padding: 1rem;
+`;
 
 const TipPost = ({ blok }) => {
+  const { content, title } = blok;
   return (
     <SbEditable content={blok} key={blok._uid}>
-      <div className="bg-white-half w-full">
-        <div className="max-w-3xl mx-auto text-center pt-20 flex flex-col items-center">
-          <h1 className="text-5xl font-bold font-serif text-primary tracking-wide">
-            {blok.title}
-          </h1>
-        </div>
-      </div>
-      <div className="max-w-3xl mx-auto text-center pt-20 flex flex-col items-center">
-        <div className="leading-relaxed text-xl text-left text-gray-800 drop-cap">
-          {render(blok.content)}
-        </div>
-      </div>
+      <Section>
+        <h1>{title}</h1>
+        <RichText content={content} />
+      </Section>
     </SbEditable>
   );
 };
