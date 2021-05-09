3a64d31c24a8c04a0dc82a58b4b4b06bd9ca438b
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-09 14:54:58 +0200

    extracted theme to another file

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,12 @@
 import { ThemeProvider } from 'styled-components'
+import Theme from '../styled/theme';
 import '../styles/tailwind.css'
 
-const theme = {
-  breakpoints: {
-    m: "1040px",
-    l: "1200px",
-  },
-  colors: {
-    title: '#3c3f40', 
-    text: '#040404',
-    link: '#7752e6'
-  },
-}
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={Theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
---
+++ styled/Theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  breakpoints: {
+    m: "1040px",
+    l: "1200px",
+  },
+  colors: {
+    title: "#3c3f40",
+    text: "#040404",
+    link: "#7752e6",
+  },
+};
+
+export default theme;
