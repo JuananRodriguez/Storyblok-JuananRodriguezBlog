d262028b10d1bc21ab217f7aeba6371c45b4d45c
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-09 20:05:01 +0200

    created codeblok component

---
+++ molecules/CodeBlock.js
@@ -0,0 +1,25 @@
+import hljs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
+import Styled from "styled-components";
+
+const Pre = Styled.pre`
+    background-color: #23241f;
+    border-radius: 8px;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    color: #fff;
+`;
+
+const CodeBlock = ({ content = "", className }) => {
+
+  const html = hljs.highlightAuto(content).value;
+    
+
+  return (
+    <Pre>
+      <code dangerouslySetInnerHTML={{ __html: html }} />
+    </Pre>
+  );
+};
+
+export default CodeBlock;
--- molecules/RichText.js
+++ molecules/RichText.js
@@ -1,19 +1,33 @@
-import { render, NODE_PARAGRAPH } from "storyblok-rich-text-react-renderer";
-import DynamicComponent from '../components/DynamicComponent'
-import ParagraphStyled from '../styled/ParagraphStyled.styled'
-
+import {
+  render,
+  NODE_PARAGRAPH,
+  NODE_CODEBLOCK,
+  MARK_CODE,
+} from "storyblok-rich-text-react-renderer";
+import DynamicComponent from "../components/DynamicComponent";
+import CodeBlock from "../molecules/CodeBlock";
+import ParagraphStyled from "../styled/ParagraphStyled.styled";
 
 const defaultBlokResolver = (component, blok) => {
-    return <DynamicComponent blok={{ component, ...blok }} />
-}
+  return <DynamicComponent blok={{ component, ...blok }} />;
+};
 
 const nodeResolvers = {
-    [NODE_PARAGRAPH]: (children) => <ParagraphStyled >{children}</ParagraphStyled>
-}
+  [NODE_PARAGRAPH]: (children) => <ParagraphStyled>{children}</ParagraphStyled>,
+  [NODE_CODEBLOCK]: (children, props) => (
+    <CodeBlock className={props.class} content={children[0]} />
+  ),
+};
 
-const RichText = ({content}) => render(content, {
+const markResolvers = {
+  [MARK_CODE]: (children) => <CodeBlock content={children} />,
+};
+
+const RichText = ({ content }) =>
+  render(content, {
     nodeResolvers,
+    markResolvers,
     defaultBlokResolver,
-})
+  });
 
 export default RichText;
