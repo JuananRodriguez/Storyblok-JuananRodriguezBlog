7a03c0e2eb572582730882542c2bb6863aac1058
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-15 19:23:25 +0200

    used richtext molecule into blogpost render instead of render rich text library

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,14 +1,17 @@
 import React from "react";
 import SbEditable from "storyblok-react";
-import { render } from "storyblok-rich-text-react-renderer";
-import { Section, H1 } from "../molecules";
+import { Section, H1, RichText } from "../molecules";
 
 const BlogPost = ({ blok }) => {
+  const { long_text, title } = blok;
+
   return (
     <SbEditable content={blok} key={blok._uid}>
       <Section>
-        <H1>{blok.title}</H1>
-        <article>{render(blok.long_text)}</article>
+        <H1>{title}</H1>
+        <article>
+          <RichText content={long_text} />
+        </article>
       </Section>
     </SbEditable>
   );
