3d2ccde105b7b7d78432ca0cd2884b416ed522cd
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-15 15:06:26 +0200

    trying to memorized time from server

--- components/Timer.js
+++ components/Timer.js
@@ -1,26 +1,34 @@
+import { memo } from "react";
+
 const Timer = ({ blok }) => {
   const { from } = blok;
-  const fromDate = new Date(new Date(from).toDateString()).getTime();
-  const toDate = new Date(new Date().toDateString()).getTime();
-
-  const diff = toDate - fromDate;
+  const fromDate = new Date(from).getTime();
+  const todayDate = new Date().getTime();
+  const diff = todayDate - fromDate;
   const totalDays = diff / (1000 * 3600 * 24);
 
   const inYears = Math.floor(totalDays / 365);
   const inMonth = Math.floor((totalDays % 365) / 30);
   const inDays = Math.floor(totalDays % 30);
+  const inHours = Math.floor((totalDays * 24) % 24);
 
   const years = inYears !== 1 ? "años" : "año";
   const months = inMonth !== 1 ? "meses" : "mes";
   const days = inDays !== 1 ? "días" : "día";
+  const hours = inHours !== 1 ? "horas" : "hora";
 
   return (
     <p>
       <strong>
-        {inYears} {years}, {inMonth} {months} y {inDays} {days}
+        {inYears} {years}, {inMonth} {months}, {inDays} {days} y {inHours}{" "}
+        {hours}
       </strong>
     </p>
   );
 };
 
-export default Timer;
+const shouldComponentUpdate = (prevProps, nextProps) => {
+  return prevProps.blok.from !== nextProps.blok.from;
+};
+
+export default memo(Timer, shouldComponentUpdate);
