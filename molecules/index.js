9103068eabe2ab8e8d460149d0e7e744a0f826c6
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-15 19:02:59 +0200

    created h1 as molecule

--- molecules/index.js
+++ molecules/index.js
@@ -2,5 +2,6 @@ import GridItem from "./GridItem";
 import Section from "./Section";
 import RichText from "./RichText";
 import CodeBlock from "./CodeBlock";
+import H1 from "./H1";
 
-export { GridItem, Section, RichText, CodeBlock };
+export { GridItem, Section, RichText, CodeBlock, H1 };
