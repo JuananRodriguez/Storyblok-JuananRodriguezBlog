07992a78742e7271fb137e6c9587b380a28f380e
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-15 10:33:34 +0200

    Codeblock is a memo component now and use language provided

--- molecules/CodeBlock.js
+++ molecules/CodeBlock.js
@@ -1,24 +1,44 @@
-import hljs from "highlight.js";
+import { memo } from "react";
+import { highlight } from "highlight.js";
 import "highlight.js/styles/monokai-sublime.css";
 import Styled from "styled-components";
 
 const Pre = Styled.pre`
+    position: relative;
     background-color: #23241f;
     border-radius: 8px;
     padding: 1rem;
     margin-bottom: 1rem;
     color: #fff;
+
+    & .CodeBlock_language{
+      position: absolute;
+      right: 1rem;
+      bottom: -1rem;
+      background-color: #23241f;
+      padding: 0.5rem 1rem;
+      border-radius: 8px;
+    }
+
 `;
 
+const shouldComponentUpdate = (prevProps, nextProps) => {
+  return (
+    prevProps.content !== nextProps.content ||
+    nextProps.className !== prevProps.className
+  );
+};
+
 const CodeBlock = ({ content = "", className }) => {
-  const html = hljs.highlightAuto(content).value;
+  const language = className.replace("language-", "");
+  const html = highlight(content, { language }).value;
 
   return (
     <Pre>
       <code dangerouslySetInnerHTML={{ __html: html }} />
+      <div className="CodeBlock_language">{language}</div>
     </Pre>
   );
-  
 };
 
-export default CodeBlock;
+export default memo(CodeBlock, shouldComponentUpdate);
