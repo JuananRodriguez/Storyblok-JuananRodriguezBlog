c04f62ad28dc5e5a4aac29b52590353790e3f40e
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-05-15 19:40:22 +0200

    fix problem with nested css styles

--- styled/GlobalStyle.styled.js
+++ styled/GlobalStyle.styled.js
@@ -1,18 +1,16 @@
 import { createGlobalStyle, css } from "styled-components";
 
 const HeadlingCommons = css`
-  ${({ theme }) => `
-      margin-top: 4rem;
-      margin-bottom: 1rem;
-      font-weight: bolder;
-      color: ${theme.colors.text};
-  `}
+  margin-top: 4.5rem;
+  margin-bottom: 1rem;
+  font-weight: bolder;
 `;
 
 const GlobalStyle = createGlobalStyle`${({ theme }) => `
     * {
       box-sizing: border-box;
       font-family: ${theme.fonts.generalFonts};
+      color: ${theme.colors.text};
       line-height: 1.5;
     }
 
